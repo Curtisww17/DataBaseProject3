5e1db0d6ee128431d1ef08df24517c2b95d030fb
Paul_Staats <paul.d.staats@gmail.com>
2019-12-03 20:49:11 -0500

    added listAllProjects
    prepared statments, statement doesn't work yet

--- src/Project3.java
+++ src/Project3.java
@@ -1,6 +1,8 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.Properties;
 
@@ -55,6 +57,39 @@ public class Project3 {
      *  @return none.
      */
     public void listAllProjects() {
+    	try {
+			PreparedStatement pstmt = conn.prepareStatement("select description,  from project");
+			// Specify the SQL query to run and execute the query. 
+			// Store the result in a ResultSet Object
+			ResultSet rst = pstmt.executeQuery();
+			// Get the metadata of the query and store it in a ResultSetMetaData object
+			ResultSetMetaData rsmd = rst.getMetaData();
+			// Get the number of columns retrieved 
+			int numberOfColumns = rsmd.getColumnCount();
+			// Go over the columns and print the name of the columns. 
+			for (int i =0; i< numberOfColumns; i++ )
+				// Note that the columns start at 1
+				System.out.print(rsmd.getColumnName(i+1)+ "\t");
+			System.out.println();
+			// Go over the rows in the ResultSet object
+			while (rst.next()) {
+				String row = "";
+				// Use getString if you are reading a varchar. 
+				// Again note that the column number starts at 1
+				// There are getInt(), getDouble(), getDate() and many other methods to read data.
+				row += rst.getString(1) + "\t";
+				row += rst.getString(2) + "\t";
+				row += rst.getString(3);
+				System.out.println(row);	
+			}
+			// Make sure you close the statement object when you are done.
+			pstmt.close();
+
+			conn.close();
+		} catch (SQLException e) {
+			System.out.println("Oops");
+		}
+
     }
     /**  This method implements: Find project by date: Find the projects (all attributes of project) 
      * that start on a particular date. 
